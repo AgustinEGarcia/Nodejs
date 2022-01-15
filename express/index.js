91118e0b69ab6e73923cb0289b8bf31867954456
AgustinEGarcia <agustinezequiel1997@mail.com>
2022-01-15 19:52:10 -0300

    rutas y algo de middlewares

--- express/index.js
+++ express/index.js
@@ -18,8 +18,34 @@ const express = require('express');
 
 const app = express();
 
+//middlewares
+
+app.use((req, res, next) => {
+    console.log('request url: ' + req.url);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('La ejecución pasó por acá');
+    next();
+});
+
+
+
+//rutas
+
 app.get('/', (req, res) => {
     res.send('Hello Word');
+});
+
+//asi se van agregando las distintas rutas de las paginas
+app.get('/login', (req, res) => {
+    res.send('Esta es la página de login');
+});
+
+//en caso que que una determinada ruta no exista, por defecto devuelve esto
+app.get('*' ,(req, res) => {
+    res.end('Archivo no encontrado');
 });
 
 app.listen(3000, () => {
