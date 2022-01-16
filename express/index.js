bac70301670dce0fb9363431c2d0315c7a17ba7d
AgustinEGarcia <agustinezequiel1997@mail.com>
2022-01-16 14:45:22 -0300

    ejemplos de uso de middleware morgan

--- express/index.js
+++ express/index.js
@@ -15,21 +15,27 @@ server.listen(3000, () => {
 
 //server creado con express
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 //middlewares
 
+app.use(morgan('short'));
+
+/*
+//este middleware muestra por consola la url que se visita
 app.use((req, res, next) => {
     console.log('request url: ' + req.url);
     next();
 });
 
+//este middleware avisa por consola que la ejecucion paso por esta parte del codigo
 app.use((req, res, next) => {
     console.log('La ejecución pasó por acá');
     next();
 });
-
+*/
 
 
 //rutas
